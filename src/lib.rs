665bda00c1f95a210b81e99f63f033869726db79
Lab <lab@pop-os.localdomain>
2024-01-17 20:16:10 +0000

    struct Piece and create_Piece fn added with signal test shape

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,31 @@ pub struct Bloxel {
     color: Color,
 }
 
+#[derive(Debug)]
+pub struct Piece {
+    x: u16,
+    y: u16,
+    color: Color,
+    shapes: Vec<[[u8; 4]; 4]>,
+}
+
+pub fn create_pieces() -> Vec<Piece> {
+    let mut pieces = Vec::new();
+
+    let mut shapes = Vec::new();
+    let shape: [[u8; 4]; 4] = [[1, 1, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]];
+    shapes.push(shape);
+    let piece: Piece = Piece {
+        x: 20,
+        y: 5,
+        color: Color::Rgb { r: 255, g: 0, b: 0 },
+        shapes,
+    };
+    pieces.push(piece);
+
+    pieces
+}
+
 pub fn create_play_area(x: u16, y: u16, bg_color: Color) -> Vec<Vec<Bloxel>> {
     let mut play_area = Vec::new();
 
@@ -38,18 +63,15 @@ pub fn create_frame(play_area: Vec<Vec<Bloxel>>) -> Vec<Vec<Color>> {
         let mut row = Vec::new();
         for y in 0..play_area[x].len() {
             row.push(play_area[x][y].color);
-            println!("{} {}", x, y);
         }
         frame.push(row);
     }
-
     // add code to render current shape
-
     frame
 }
 
 pub fn render_frame(frame: &Vec<Vec<Color>>) -> io::Result<()> {
-    // should check and throw error if terminal is smaller than framei
+    // should check and throw error if terminal is smaller than frame
     let mut stdout = io::stdout();
 
     execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
